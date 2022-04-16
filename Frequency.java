90d58cea0f7d9bb7a4e578fdc13aeaa4c4f6c5d0
classical-coding <102431387+classical-coding@users.noreply.github.com>
2022-04-16 22:59:14 +0530

    removing blank space

--- Frequency.java
+++ Frequency.java
@@ -10,8 +10,7 @@ public class Frequency {
 		List<Integer> inputList = Arrays.asList(1,7,1,1,2,-99,2,3,9,3,11,19,5,9,11,30,-99,8,1,7);
 		
 		/* finding occurrence of each element in the list and putting it in the Map */
-		Map<Integer,Integer> outputMap = inputList.stream()
-											.collect(Collectors.toMap(k-> k, v-> 1, Integer::sum));
+		Map<Integer,Integer> outputMap = inputList.stream().collect(Collectors.toMap(k-> k, v-> 1, Integer::sum));
 		
 		/* displaying the output */
 		outputMap.forEach((k,v)->{
