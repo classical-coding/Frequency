16560d6d5200b2f5cbfeceef65365791af48a45a
classical-coding <102431387+classical-coding@users.noreply.github.com>
2022-04-16 22:58:36 +0530

    initial commit

---
+++ Frequency.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Frequency {
+	public static void main(String[] args) {
+		
+		/* creating a list with dummy data */
+		List<Integer> inputList = Arrays.asList(1,7,1,1,2,-99,2,3,9,3,11,19,5,9,11,30,-99,8,1,7);
+		
+		/* finding occurrence of each element in the list and putting it in the Map */
+		Map<Integer,Integer> outputMap = inputList.stream()
+											.collect(Collectors.toMap(k-> k, v-> 1, Integer::sum));
+		
+		/* displaying the output */
+		outputMap.forEach((k,v)->{
+			System.out.println(k+" occurs "+v+" times");
+		}); 							
+	}
+}
